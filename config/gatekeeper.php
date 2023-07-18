46022a4ca8278f1e866109da168773d887023a2b
Abdalrhman Emad Saad <kettasoft@gmail.com>
2023-07-18 16:31:50 +0300

    This is the array that contains the information of the user models.

--- config/gatekeeper.php
+++ config/gatekeeper.php
@@ -1,5 +1,20 @@
 <?php
 
 return [
-    //
+    /*
+    |--------------------------------------------------------------------------
+    | Gatekeeper User Models
+    |--------------------------------------------------------------------------
+    |
+    | This is the array that contains the information of the user models.
+    | This information is used in the add-trait command, for the roles and
+    | permissions relationships with the possible user models, and the
+    | administration panel to add roles and permissions to the users.
+    |
+    | The key in the array is the name of the relationship inside the roles and permissions.
+    |
+    */
+    'user_models' => [
+        'users' => App\Models\User::class
+    ],
 ];
