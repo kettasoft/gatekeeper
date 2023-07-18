44940bb582b9f83b47bceb42ce2182aedd5ab000
Abdalrhman Emad Saad <kettasoft@gmail.com>
2023-07-18 20:57:24 +0300

    Command run migrations handling for gatekeeper

---
+++ src/Commands/RunMigraitonsCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Kettasoft\Gatekeeper\Commands;
+
+use Illuminate\Console\Command;
+
+class RunMigraitonsCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'gatekeeper:migrate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Run gatekeeper migrations';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->call('migrate', [
+            '--path' => '\gatekeeper\database\migrations'
+        ]);
+    }
+}
