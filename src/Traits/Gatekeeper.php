ea727c83e6742a2e8eaf6dcdceb3f858db04aaaf
Abdalrhman Emad Saad <kettasoft@gmail.com>
2023-07-24 11:59:07 +0300

    Create the basic trait and defining relationships and the gatekeeper assigner trait

---
+++ src/Traits/Gatekeeper.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Kettasoft\Gatekeeper\Traits;
+
+use Kettasoft\Gatekeeper\Traits\GatekeeperAssigner;
+
+trait Gatekeeper
+{
+    use GatekeeperUserRelations;
+    use GatekeeperAssigner;
+}
