cdd39e52d5befd8407d9aa5097eaffeeff8d56b1
Abdalrhman Emad Saad <kettasoft@gmail.com>
2023-07-27 12:42:27 +0300

    Refactor assigner class and add current user remove permission method

--- src/Services/Assigner.php
+++ src/Services/Assigner.php
@@ -5,10 +5,14 @@ namespace Kettasoft\Gatekeeper\Services;
 use Illuminate\Support\Collection;
 use Kettasoft\Gatekeeper\Contracts\AssignerInterface;
 use Kettasoft\Gatekeeper\Contracts\GatekeeperInterface;
-use Kettasoft\Gatekeeper\Validation\Validators\CheckPermissionIfNotHaveMultipleDimensions;
+use Kettasoft\Gatekeeper\Traits\GatekeeperCacheing;
+use Illuminate\Support\Arr;
 
 class Assigner implements AssignerInterface
 {
+
+    use GatekeeperCacheing;
+
     public function __construct(protected GatekeeperInterface $user)
     {
     }
@@ -24,21 +28,36 @@ class Assigner implements AssignerInterface
      * @param string|array $permission
      * @return GatekeeperInterface
      */
-    public function currentUserGivePermission(string|array $permission): true
+    public function currentUserGivePermission(string|array $permission): GatekeeperInterface
     {
-
         $permission = $this->setDefaultValueToPermission($permission);
+        $userCachedPermissions = collect($this->userCachedPermissions())->get('permissions');
 
-        if ($this->user->permissions->isEmpty()) {
+        if (is_null($userCachedPermissions)) {
             $this->user->permissions()->create(['permissions' => $permission]);
-            return true;
+            return $this->user;
         }
 
-        $newPermissions = array_values($this->allPermissions()->add($permission)->flatten()->unique()->toArray());
+        $newPermissions = array_merge($userCachedPermissions, $permission);
 
         $this->user->permissions()->update(['permissions' => $newPermissions]);
 
-        return true;
+        return $this->user;
+    }
+
+    public function currentUserRemovePermission(array|string $permission): GatekeeperInterface
+    {
+        if ($cachedPermissions = $this->userCachedPermissions()) {
+            $newPermissions = collect(Arr::get($cachedPermissions, 'permissions'))->forget($permission);
+
+            if ($newPermissions->isEmpty()) {
+                $this->user->permissions()->delete();
+            }
+
+            $this->user->permissions()->update(['permissions' => $newPermissions]);
+        }
+
+        return $this->user;
     }
 
     private function setDefaultValueToPermission(array $permissions, $default = true)
