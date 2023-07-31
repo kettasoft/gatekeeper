04c9d1fd05ffcbb377429d97092e0620924b7c1a
Abdalrhman Emad Saad <kettasoft@gmail.com>
2023-07-31 14:07:32 +0300

    Setup the configuration for Gatekeeper.

--- src/Providers/GatekeeperServiceProvider.php
+++ src/Providers/GatekeeperServiceProvider.php
@@ -23,7 +23,18 @@ class GatekeeperServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        $this->configure();
         $this->commands(RunMigraitonsCommand::class);
+    }
+
+    /**
+     * Setup the configuration for Gatekeeper.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this->mergeConfigFrom(__DIR__.'/../../config/gatekeeper.php', 'gatekeeper');
     }
 
 }
