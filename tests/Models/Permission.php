225ac3606005c708ab1d3a5bd6a1a77209141009
Abdalrhman Emad Saad <kettasoft@gmail.com>
2023-08-01 14:57:41 +0300

    Permission model test

---
+++ tests/Models/Permission.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Gatekeeper\Tests\Models;
+
+use Kettasoft\Gatekeeper\Models\Permission as PermissionModel;
+
+class Permission extends PermissionModel
+{
+    protected $guarded = [];
+}
